1f6711bedf6c1d62c3336d1cfcd45fc2ad44f096
johnhalsteaduk <johnhalstead@outlook.com>
2026-08-15 11:56:10 +0100

    stop tracking +main folder

--- +main/+sparse/+block/dynamic_1.m
+++
@@ -1,8 +0,0 @@
-function [y, T] = dynamic_1(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(23)=params(6)*y(8)+x(1);
-  y(27)=x(1);
-  y(28)=y(12);
-  y(29)=y(13);
-  y(30)=y(14);
-  y(26)=x(1)+y(12)+y(13)+y(14)+y(15);
-end
--- +main/+sparse/+block/static_2.m
+++
@@ -1,7 +0,0 @@
-function [y, T] = static_2(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(12)=x(1);
-  y(13)=y(12);
-  y(14)=y(13);
-  y(15)=y(14);
-  y(11)=x(1)+y(12)+y(13)+y(14)+y(15);
-end
